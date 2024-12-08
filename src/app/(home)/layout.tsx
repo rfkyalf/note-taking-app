dab8928e8bb39b10cedcf9cbc3d7befd6cfe2d2c
Rifky Alfarez <207006052@student.unsil.ac.id>
2024-12-08 17:32:19 +0700

    refactor: refactor how it works section

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,5 +1,6 @@
 import HomeNavbar from '@/components/home/HomeNavbar';
 import Wrapper from '@/components/Wrapper';
+import Link from 'next/link';
 
 export default function HomeLayout({
   children,
@@ -10,6 +11,17 @@ export default function HomeLayout({
     <div className="min-h-screen bg-neutral-50 flex flex-col">
       <HomeNavbar />
       <Wrapper className="min-h-screen">{children}</Wrapper>
+      <footer className="border-t border-neutral-300 py-4 flex justify-center">
+        <p className="text-[0.9rem] text-neutral-700">
+          Created by{' '}
+          <Link
+            href="https://www.rifkyalfarez.my.id"
+            className="font-bold hover:underline"
+          >
+            Rifky Alfarez
+          </Link>
+        </p>
+      </footer>
     </div>
   );
 }
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,54 +1,13 @@
 import FeaturesSection from '@/components/home/FeaturesSection';
 import HeroSection from '@/components/home/HeroSection';
-import SectionHeader from '@/components/home/SectionHeader';
-import { HOW_IT_WORKS } from '@/lib/constanst';
+import HowItWorksSection from '@/components/home/HowItWorksSection';
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <FeaturesSection />
-      <section className="my-10 md:my-0 md:h-screen flex flex-col justify-center items-center gap-y-6 md:gap-y-10">
-        <SectionHeader
-          section_name="How it Works"
-          title="Simple Steps to Save Your Notes"
-          desc="Discover how easy it is to manage your notes with just a few simple steps. Create, organize, and access your notes anytime, anywhere—staying productive has never been this effortless."
-        />
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-10">
-          {HOW_IT_WORKS.map((how) => (
-            <div
-              key={how.id}
-              className="flex flex-col gap-y-4 border border-neutral-300 rounded-md p-2 md:p-4"
-            >
-              <p className="bg-neutral-200 w-fit text-[0.8rem] text-neutral-700 px-2 border border-neutral-300 rounded">
-                {how.id}
-              </p>
-              <div>
-                <h3 className="text-[0.9rem] md:text-[1rem] text-neutral-900 font-medium">
-                  {how.title}
-                </h3>
-                <p className="text-[0.8rem] md:text-[0.9rem] text-neutral-700">
-                  {how.desc}
-                </p>
-              </div>
-            </div>
-          ))}
-        </div>
-        <div className="relative w-full bg-neutral-950 flex flex-col items-center gap-y-4 rounded-md p-2 md:p-4">
-          <div className="absolute inset-0 h-full w-full bg-[linear-gradient(to_right,#40404040_1px,transparent_1px),linear-gradient(to_bottom,#40404040_1px,transparent_1px)] bg-[size:20px_20px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_80%,transparent_100%)]" />
-          <div className="relative">
-            <h3 className="text-[1rem] text-neutral-100 font-semibold text-center">
-              Ready to Get Started?
-            </h3>
-            <p className="text-[0.8rem] text-neutral-400 text-center">
-              Create your account and start saving your notes today!
-            </p>
-          </div>
-          <button className="relative w-fit bg-neutral-200 text-[0.8rem] text-neutral-900 px-4 py-1 rounded">
-            Sign Up
-          </button>
-        </div>
-      </section>
+      <HowItWorksSection />
     </main>
   );
 }
