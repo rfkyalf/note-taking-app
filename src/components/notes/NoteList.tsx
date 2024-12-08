0ec28ec8e23c3b0337998db0415187f2d214e2b7
Rifky Alfarez <207006052@student.unsil.ac.id>
2024-12-08 07:47:19 +0700

    feat(notes): add link to home

--- src/components/notes/NoteList.tsx
+++ src/components/notes/NoteList.tsx
@@ -30,41 +30,46 @@ export default async function NoteList({
           No notes found
         </p>
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4">
-          {notes?.map((note) => (
-            <div
-              key={note.id}
-              className="w-full bg-neutral-50 flex flex-col justify-between shadow-md rounded-md p-2 md:p-4"
-            >
-              <div className="flex flex-col">
-                <p className="text-[0.6rem] md:text-[0.7rem] text-neutral-400 self-end mb-1">
-                  {formatRelativeDate(note.updatedAt.toISOString())}
-                </p>
-                <h3 className="text-[1rem] md:text-[1.1rem] text-neutral-900 font-medium">
-                  <Link href={`/notes/${note.id}`} className="hover:underline">
-                    {note.title}
-                  </Link>
-                </h3>
-                <p className="text-[0.8rem] md:text-[0.9rem] text-neutral-700 text-pretty whitespace-pre-wrap max-h-[150px] overflow-auto">
-                  {note.content}
-                </p>
-              </div>
-              <div className="flex items-center justify-between mt-2 md:mt-4">
-                <p
-                  className={`w-fit text-[0.7rem] md:text-[0.8rem] text-neutral-900 px-2 rounded ${categoryColor(
-                    note.category
-                  )}`}
-                >
-                  {note.category}
-                </p>
-                <div className="flex items-center gap-x-1 md:gap-x-2">
-                  <UpdateNote privateNote={note} />
-                  <DeleteNote id={note.id} />
+        <>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4">
+            {notes?.map((note) => (
+              <div
+                key={note.id}
+                className="w-full bg-neutral-50 flex flex-col justify-between shadow-md rounded-md p-2 md:p-4"
+              >
+                <div className="flex flex-col">
+                  <p className="text-[0.6rem] md:text-[0.7rem] text-neutral-400 self-end mb-1">
+                    {formatRelativeDate(note.updatedAt.toISOString())}
+                  </p>
+                  <h3 className="text-[1rem] md:text-[1.1rem] text-neutral-900 font-medium">
+                    <Link
+                      href={`/notes/${note.id}`}
+                      className="hover:underline"
+                    >
+                      {note.title}
+                    </Link>
+                  </h3>
+                  <p className="text-[0.8rem] md:text-[0.9rem] text-neutral-700 text-pretty whitespace-pre-wrap max-h-[150px] overflow-auto">
+                    {note.content}
+                  </p>
+                </div>
+                <div className="flex items-center justify-between mt-2 md:mt-4">
+                  <p
+                    className={`w-fit text-[0.7rem] md:text-[0.8rem] text-neutral-900 px-2 rounded ${categoryColor(
+                      note.category
+                    )}`}
+                  >
+                    {note.category}
+                  </p>
+                  <div className="flex items-center gap-x-1 md:gap-x-2">
+                    <UpdateNote privateNote={note} />
+                    <DeleteNote id={note.id} />
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        </>
       )}
     </>
   );
--- src/components/notes/NotesNavbar.tsx
+++ src/components/notes/NotesNavbar.tsx
@@ -2,6 +2,7 @@
 
 import { UserButton, useUser } from '@clerk/nextjs';
 import Wrapper from '../Wrapper';
+import Link from 'next/link';
 
 export default function NotesNavbar() {
   const { isLoaded } = useUser();
@@ -9,7 +10,9 @@ export default function NotesNavbar() {
   return (
     <div className="bg-neutral-50 shadow py-4">
       <Wrapper className="flex items-center justify-between">
-        <h1 className="text-[1rem] text-neutral-950 font-bold">Notes App</h1>
+        <h1 className="text-[1rem] text-neutral-950 font-bold">
+          <Link href="/">Notes App</Link>
+        </h1>
         {isLoaded ? (
           <UserButton />
         ) : (
